16db2b5172f3b1de9d6c8f4ac34a436ab4d58df0
rhodlib <rhodlib@gmail.com>
2024-07-23 19:48:53 +0000

    SafeAreaViewContext added, and animations with Animated

--- metacritic-app/App.js
+++ metacritic-app/App.js
@@ -1,39 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-} from "react-native";
-import Constants from "expo-constants";
-import { useEffect, useState } from "react";
-import { getLatestGames } from "./lib/metacritic";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Main } from "./components/Main";
 
 export default function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    getLatestGames().then((games) => setGames(games));
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <SafeAreaView style={{ margin: 12 }}>
-        <ScrollView>
-          {games.map((game) => (
-            <View key={game.slug} style={styles.card}>
-              <Image source={{ uri: game.image }} style={styles.image} />
-              <Text style={styles.title}>{game.title}</Text>
-              <Text style={styles.score}>{game.score}</Text>
-              <Text style={styles.description}>{game.description}</Text>
-            </View>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -43,17 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: Constants.statusBarHeight,
-    padding: 12,
-  },
-  card: { marginBottom: 42 },
-  image: { width: 107, height: 147, borderRadius: 10 },
-  title: { fontSize: 20, fontWeight: "bold", color: "#fff", marginTop: 10 },
-  description: { fontSize: 16, color: "#eee" },
-  score: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "green",
-    marginBottom: 10,
+    paddingHorizontal: 12,
   },
 });
---
+++ metacritic-app/components/Main.jsx
@@ -0,0 +1,34 @@
+import { View, FlatList, ActivityIndicator } from "react-native";
+import { useEffect, useState } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { getLatestGames } from "../lib/metacritic";
+import { AnimatedGameCard } from "./GameCard";
+import { Logo } from "./Logo";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games) => setGames(games));
+  }, []);
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 20 }}>
+        <Logo />
+      </View>
+      {games.length === 0 ? (
+        <ActivityIndicator color={"#FFF"} size={"large"} />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard game={item} index={index} />
+          )}
+        />
+      )}
+    </View>
+  );
+}
